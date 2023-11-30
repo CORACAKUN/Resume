1630260a3a75fdfa79a49a9cf3bdd6e6f30496f3
CORACA <114158929+CORACAKUN@users.noreply.github.com>
2023-11-30 22:17:37 +0800

    Add files via upload

---
+++ Resume/index.php
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Resume</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Mooli&family=Roboto:ital,wght@0,100;0,400;0,900;1,100;1,300;1,500;1,700&display=swap" rel="stylesheet">
+
+</head>
+<body>
+    
+    <main>
+        <section class="left-section">
+            <div class="profile-div">
+                <img src="profile/coraca profile.jpg" alt="CORACA">
+                <h1>Ramon Carl Conda</h1>
+                <h2>Software Developer</h2>
+                <div class="divider" id="profile-divider"></div>
+            </div>
+            <div class="place-div">
+                <div class="left-pdiv">
+                    <p>Country <br> Province <br> City <br> Age</p>
+                </div>
+                <div class="right-pdiv">
+                    <p>Philippines <br> Sorsogon <br> Bulan <br>21</p>
+                </div>
+                <div class="divider" id="place-divider"></div>
+            </div>
+            <div class="language-div">
+                <p>Language</p>
+                <div class="langs">
+                    <p>Filipino</p>
+                    <p>English</p>
+                </div>
+                <div class="divider" id="language-divider"></div>
+            </div>
+            <div class="pl-div">
+                <p class="pl">Programming Language</p>
+                <div class="pls">
+                    <div class="pls-icons">
+                        <img class="html" src="pl images/html icon png.png" alt="">
+                        <img class="css" src="pl images/css icon png.png" alt="">
+                        <img class="js" src="pl images/js logo png.png" alt="">
+                    </div>
+                    <div class="pls-langs">
+                        <p class="htmlt">HTML</p>
+                        <p class="csst">CSS</p>
+                        <p class="jst">JAVASCRIPT</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section class="right-section">
+            <div class="top-div"></div>
+            <div class="center-div">
+                <div class="school">
+                    <h4>School</h4>
+                    <p>
+                        San Francisco  Elementary School (Bulan Sorsogon) <br>
+                        San Francisco National High School (Bulan Sorsogon) <br>
+                        Sorsogon State University (Bulan Campus)
+                    </p>
+                </div>
+                <div class="divider"></div>
+                <div class="jobs">
+                    <h4>Jobs</h4>
+                    <p>Merchandiser &#8211; Cellphone and Computer Technician &#8211; Software/Application/OS Installer &#8211; Sounds System Operator &#8211; Motorcycle Mechanic &#8211; Freelancing &#8211; Programmer
+                    </p>
+                </div>
+            </div>  
+            <div class="bottom-div">
+                <div class="completed-projects">
+                    <div class="title">
+                        <h3>COMPLETED PROJECTS</h3>
+                    </div>
+                    <div class="html-css">
+                        <div class="images">
+                            <img class="htmli" src="pl images/html icon png.png" alt="">
+                            <img class="cssi" src="pl images/css icon png.png" alt="">
+                            <img class="jsi" src="pl images/js logo png.png" alt="">
+                        </div>
+                        <div class="projects">
+                            <ul>
+                                <li>The Planetarium Game</li>
+                                <li>Mouse Maze Game</li>
+                                <li>Portfolio Website</li>
+                                <li>Voucher Generator</li>
+                                <li>Currency Converter</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="java">
+                        <div class="img">
+                            <img class="java-icons" src="/pl images/java icon logo.png" alt="">
+                        </div>
+                        <div class="java-projects">
+                            <ul>
+                                <li>Digital Clock</li>
+                                <li>Banking System</li>
+                                <li>User Authentication</li>
+                                <li>Currency Converter</li>
+                                <li>Calculator</li>
+                                <li>Age Calculator</li>
+                                <li>BMI Calculator</li>
+                                <li>Time Calculator</li>
+                                <li>Grade Calculator</li>
+                                <li>Area And Perimeter Calculator</li>
+                                <li>Random Password Generator</li>
+                                <li>Note App</li>
+                                <li>Recipe App</li>
+                                <li>Guessing Game</li>
+                                <li>Quiz Game</li>
+                                <li>Tic Tac Toe Game</li>
+                                <li>Fil-Bicol Dictionary</li>
+                                <li>Digital Wallet</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+</body>
+</html>
